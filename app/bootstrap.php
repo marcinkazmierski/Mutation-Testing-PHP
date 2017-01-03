afb4246aa51478f85aefd26f66083815be3b6c06
Marcin Kaźmierski <marcin.ryszard.kazmierski@gmail.com>
2017-01-03 20:12:10 +0100

    Added console file and bootstrap.

---
+++ app/bootstrap.php
@@ -0,0 +1,17 @@
+<?php
+declare(strict_types = 1);
+
+if (!defined('APPLICATION_ROOT')) {
+    define('APPLICATION_ROOT', getcwd());
+}
+
+spl_autoload_register('autoLoader');
+function autoLoader($class_name)
+{
+    $class = str_replace('\\', DIRECTORY_SEPARATOR, $class_name) . '.php';
+    $fileClass = APPLICATION_ROOT . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR . $class;
+
+    if (file_exists($fileClass)) {
+        require_once $fileClass;
+    }
+}
